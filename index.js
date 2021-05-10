fb839d4b2787ed414ce4cb16305545451832c01f
eddrickk <eddricklumine@gmail.com>
2021-05-10 22:40:58 +0700

    Installed Cors. Configured

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 var mysql = require('mysql')
+var cors = require('cors')
+
+const corsOptions ={
+    origin:'*',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
 const app = express()
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded())
 
