b674251faa797076b31d40f60a3e5eeb27121253
eddrickk <eddricklumine@gmail.com>
2021-05-10 22:17:28 +0700

    MySQL Connect, get and post method

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
+var mysql = require('mysql')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todo_development'
+  })
+
+connection.connect()
 app.get('/', function (req, res) {
     res.send(`
         <html>
             <form action="/todo" method="POST">
-                <label>nama</label>
+                <label>Description</label>
                 <input name = "description"></input>
-                <button>lanjut</lanjut>
+                <button>Submit</button>
             </form>
         </html>
     `)
@@ -19,6 +28,28 @@ app.get('/', function (req, res) {
 
 app.post('/todo', function (req, res) {
     console.log('Got body:', req.body)
+    connection.query(`INSERT INTO item (description) VALUES (\'${req.body.description}\')`, function (err, rows, fields) {
+        if (err) throw err
+      
+        console.log('Data insert successful')
+      })
+    
+    res.sendStatus(200)
+})
+
+app.get('/todo', function (req, res) {
+    const query = 'SELECT * FROM item'
+    connection.query(query, (err, table) => {
+        if (err){
+            console.error(err)
+            return
+        }
+        for (let row of table){
+            console.log(row.id)
+        }
+
+        res.send(table)
+    })
 })
 
 app.listen(3000, () => {
