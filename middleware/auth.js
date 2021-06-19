59d9b4dabb7da561c1411fedf82c1234e32af747
eddrickk <eddricklumine@gmail.com>
2021-06-19 20:03:15 +0700

    proses dekripsi password di auth.js done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql')
+const bcrypt = require('bcrypt')
+
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -12,14 +14,20 @@ module.exports = function (req, res, next) {
     const username = req.headers.username
     const password = req.headers.password
 
-    const query = `SELECT * FROM users WHERE username=\'${username}\' AND password=\'${password}\'`
-    connection.query(query, (err, table) => {
+    const query = `SELECT * FROM users WHERE username=\'${username}\'`
+    connection.query(query, async (err, table) => {
         if (err){
             console.error(err)
             return
         }
         if (table.length > 0){
-            next()
+            const compResult = await bcrypt.compare(password, table[0].password)
+            if (compResult){
+                next()
+            }
+            else{
+                res.send(401)
+            }
         }
         else{
             res.send(401)
